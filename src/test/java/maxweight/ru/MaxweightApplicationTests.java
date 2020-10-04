75fdb42815e273ed4102f0b6e3cca3625035046d
IgorIF <igarif1992@gmail.com>
2020-10-05 01:59:21 +0300

    add project

---
+++ src/test/java/maxweight/ru/MaxweightApplicationTests.java
@@ -0,0 +1,13 @@
+package maxweight.ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxweightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
