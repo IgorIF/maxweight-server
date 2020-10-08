cf4ff0578b64058a91e431eb08a162856be1a0a7
IgorIF <igarif1992@gmail.com>
2020-10-08 19:14:15 +0300

    remove RoleRepository.java

--- src/main/java/maxweight/ru/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package maxweight.ru.repositories;
-
-import maxweight.ru.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
